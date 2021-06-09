5dc8761878f68cf4783c720575218bae6780bdc0
lukaszsliwinski <lukaszsliwinski0@gmail.com>
2021-06-09 17:59:15 +0200

    Allow to create task with no due date

--- base/models.py
+++ base/models.py
@@ -9,7 +9,7 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateField(default=datetime.date.today)
+    due_date = models.DateField(default=datetime.date.today, null=True, blank=True)
 
     def __str__(self):
         return self.title
